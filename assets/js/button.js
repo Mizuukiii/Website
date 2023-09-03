1f1590312b30a755a7cdf6c7488c236c635f6662
Mizuki <95535899+Mizuukiii@users.noreply.github.com>
2023-09-04 00:07:14 +0800

    Update button.js

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,3 +1,1 @@
-document.getElementById('back-button').addEventListener('click', function() {
-    window.location.href = 'index.html'; // Replace 'index.html' with the actual path to your index page
-});
+

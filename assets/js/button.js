c53eabe5ef7ee5e79e58283ea736610e9b3667f8
Mizuki <95535899+Mizuukiii@users.noreply.github.com>
2023-09-03 23:55:03 +0800

    Add files via upload

---
+++ assets/js/button.js
@@ -0,0 +1,3 @@
+document.getElementById('back-button').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Replace 'index.html' with the actual path to your index page
+});
